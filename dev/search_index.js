96ab0bc0010cafbcfec8997784cde83b8f8f4976
Documenter.jl <documenter@juliadocs.github.io>
2022-08-28 12:08:59 +0000

    build based on 697b1da

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MulticomplexNumbers.jl","page":"MulticomplexNumbers.jl","title":"MulticomplexNumbers.jl","text":"","category":"section"},{"location":"","page":"MulticomplexNumbers.jl","title":"MulticomplexNumbers.jl","text":"Documentation for MulticomplexNumbers.jl","category":"page"}]
+}
